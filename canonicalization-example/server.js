1a4925c6e53e8fc3f9c9b6c2b5005db18b193de4
Mohammed Beshir <mbeshir2720@gmail.com>
2025-12-10 23:52:48 -0500

    Update server.js

--- canonicalization-example/server.js
+++ canonicalization-example/server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const { body, validationResult } = require('express-validator');
+const helmet = require("helmet");
 
 const app = express();
+app.use(express.urlencoded({extended: false}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
